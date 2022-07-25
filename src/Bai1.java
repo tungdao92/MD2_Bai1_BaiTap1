9bcb79046b38b0c536dfe40a9a4a215096fcdbeb
unknown <tungdao923@gmail.com>
2022-07-25 11:30:14 +0700

    bai1

---
+++ src/Bai1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập tên");
+        String name = scanner.nextLine();
+        System.out.println("Xin chào " + name);
+    }
+}
